c969fd92cd2a7bb009d4ffac68d8ec342fa931a1
nathan1658 <>
2022-03-21 23:05:37 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <game-board/>
+  </div>
+</template>
+
+<script>
+import GameBoard from './components/GameBoard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GameBoard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="game-board">
+    <div class="box" v-for="n in 9" :key="n" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.game-board {
+  background: grey;
+  height: 500px;
+  width: 500px;
+  margin: auto;
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(3, 1fr);
+}
+
+.box {
+  padding: 10px;
+  background-color: lightblue;
+}
+</style>
